31922d817e7eaecb72a6ac024cc2b0e758961978
Jorge Hidalgo <jorge.hidalgo@gmail.com>
2014-06-02 21:12:24 +0200

    updates to support multi-language analysis and independence from unit test execution in SonarQube

---
+++ mvn-verify-coverage.bat
@@ -0,0 +1,1 @@
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify -P coverage-per-test,cargo-tomcat,selenium-tests,jmeter-tests
---
+++ mvn-verify.bat
@@ -0,0 +1,1 @@
+mvn clean verify -P cargo-tomcat,selenium-tests,jmeter-tests
