1436dac06f426548863f14c510184530f65de3af
Jorge Hidalgo <jorge.hidalgo@accenture.com>
2014-06-09 15:29:09 +0200

    fixed quality issues

--- src/main/java/org/springframework/samples/petclinic/validation/OwnerValidator.java
+++ src/main/java/org/springframework/samples/petclinic/validation/OwnerValidator.java
@@ -12,32 +12,36 @@ import org.springframework.validation.Errors;
  */
 public class OwnerValidator {
 
-	public void validate(Owner owner, Errors errors) {
-		if (!StringUtils.hasLength(owner.getFirstName())) {
-			errors.rejectValue("firstName", "required", "required");
-		}
-		if (!StringUtils.hasLength(owner.getLastName())) {
-			errors.rejectValue("lastName", "required", "required");
-		}
-		if (!StringUtils.hasLength(owner.getAddress())) {
-			errors.rejectValue("address", "required", "required");
-		}
-		if (!StringUtils.hasLength(owner.getCity())) {
-			errors.rejectValue("city", "required", "required");
-		}
+    public OwnerValidator() {
+        super();
+    }
 
-		String telephone = owner.getTelephone();
-		if (!StringUtils.hasLength(telephone)) {
-			errors.rejectValue("telephone", "required", "required");
-		}
-		else {
-			for (int i = 0; i < telephone.length(); ++i) {
-				if ((Character.isDigit(telephone.charAt(i))) == false) {
-					errors.rejectValue("telephone", "nonNumeric", "non-numeric");
-					break;
-				}
-			}
-		}
-	}
+    public void validate(Owner owner, Errors errors) {
+        if (!StringUtils.hasLength(owner.getFirstName())) {
+            errors.rejectValue("firstName", "required", "required");
+        }
+        if (!StringUtils.hasLength(owner.getLastName())) {
+            errors.rejectValue("lastName", "required", "required");
+        }
+        if (!StringUtils.hasLength(owner.getAddress())) {
+            errors.rejectValue("address", "required", "required");
+        }
+        if (!StringUtils.hasLength(owner.getCity())) {
+            errors.rejectValue("city", "required", "required");
+        }
+
+        String telephone = owner.getTelephone();
+        if (!StringUtils.hasLength(telephone)) {
+            errors.rejectValue("telephone", "required", "required");
+        }
+        else {
+            for (int i = 0; i < telephone.length(); ++i) {
+                if (!Character.isDigit(telephone.charAt(i))) {
+                    errors.rejectValue("telephone", "nonNumeric", "non-numeric");
+                    break;
+                }
+            }
+        }
+    }
 
 }
