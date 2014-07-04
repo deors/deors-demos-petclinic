3438eb9c46fb5964d155c2a08e8a0943ab70b6f7
Jorge Hidalgo <jorge.hidalgo@gmail.com>
2014-07-04 23:56:58 +0200

    updated versions of JaCoCo and SonarQube JaCoCo plug-in

--- mvn-verify-coverage.bat
+++ mvn-verify-coverage.bat
@@ -1,1 +1,1 @@
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify -P coverage-per-test,cargo-tomcat,selenium-tests,jmeter-tests
+mvn clean org.jacoco:jacoco-maven-plugin:0.7.0.201403182114:prepare-agent verify -P coverage-per-test,cargo-tomcat,selenium-tests,jmeter-tests
