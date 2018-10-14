0f0d647ef2df1aa34c952e08ace4f8c76699c120
Jorge Hidalgo <jorge.hidalgo@gmail.com>
2018-10-14 14:36:48 +0200

    add missing package-info files

---
+++ src/main/java/org/springframework/samples/petclinic/aspects/package-info.java
@@ -0,0 +1,6 @@
+/**
+ *
+ * The classes in this package are utility aspects used by other components.
+ *
+ */
+package org.springframework.samples.petclinic.aspects;
---
+++ src/main/java/org/springframework/samples/petclinic/util/package-info.java
@@ -0,0 +1,6 @@
+/**
+ *
+ * The classes in this package provide with common utility functions.
+ *
+ */
+package org.springframework.samples.petclinic.util;
